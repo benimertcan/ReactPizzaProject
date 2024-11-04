3bdbe664cece3f123f4da3ac34bf25b02bc4f974
Mertcan Kurt <mertcankurt25@gmail.com>
2024-11-04 17:43:22 +0300

    orderPage.jsx added

---
+++ src/components/OrderPage.jsx
@@ -0,0 +1,23 @@
+import '../App.css'
+
+function OrderPage() {
+
+
+  return (
+    <>
+     <div>
+        <div className='header'>
+            <img src='public\homepage\logo.svg'/>
+        <div>
+            <a></a>
+        </div>
+        </div>
+
+
+
+     </div>
+    </>
+  )
+}
+
+export default OrderPage
