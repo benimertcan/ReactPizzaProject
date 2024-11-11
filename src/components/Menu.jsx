a826796e25bb59f907cd8db91361668242b55a30
Mertcan Kurt <mertcankurt25@gmail.com>
2024-11-12 00:27:14 +0300

    changing image's paths for vercel

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -5,12 +5,12 @@ import './HomePage.css'
 
 function Menu() {
     const icons = [
-        { url: "public/products/icons/1.svg", name: "YENİ! Kore" },
-        { url: "public/products/icons/2.svg", name: "Pizza" },
-        { url: "public/products/icons/3.svg", name: "Burger" },
-        { url: "public/products/icons/4.svg", name: "Kızartlar" },
-        { url: "public/products/icons/5.svg", name: "Fast Food" },
-        { url: "public/products/icons/6.svg", name: "Gazlı İçecek" },
+        { url: "/public/products/icons/1.svg", name: "YENİ! Kore" },
+        { url: "/public/products/icons/2.svg", name: "Pizza" },
+        { url: "/public/products/icons/3.svg", name: "Burger" },
+        { url: "/public/products/icons/4.svg", name: "Kızartlar" },
+        { url: "/public/products/icons/5.svg", name: "Fast Food" },
+        { url: "/public/products/icons/6.svg", name: "Gazlı İçecek" },
     ];
     return (
         <>
